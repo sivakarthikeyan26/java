4e123b28b0cd0b2567d74bca6b0184770a689f84
Your Name <you@example.com>
2022-06-20 17:55:01 +0530

    added line for creating conflicts

--- arithmetic_operations/src/arithmetic_operations/add1.java
+++ arithmetic_operations/src/arithmetic_operations/add1.java
@@ -4,6 +4,7 @@ public class add1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("this is for creating conflicts");
 		System.out.println("hello");
 		System.out.println("this is new line");
 
