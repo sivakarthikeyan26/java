8fd50db3d58381a12f14a802e453dc6eae34c88d
Your Name <you@example.com>
2022-06-20 18:53:10 +0530

    added two lines

--- arithmetic_operations/src/arithmetic_operations/add1.java
+++ arithmetic_operations/src/arithmetic_operations/add1.java
@@ -3,10 +3,11 @@ package arithmetic_operations;
 public class add1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a,b,c; 
+		// TODO Auto-generated method
 		System.out.println("hello");
 		System.out.println("this is new line");
+		yellow
+		red
 
 	}
 
