95843cc6ed5e26df2829487c93f74a5cf0030ef3
Your Name <you@example.com>
2022-06-20 18:20:07 +0530

    initialized variables in line 7

--- arithmetic_operations/src/arithmetic_operations/add1.java
+++ arithmetic_operations/src/arithmetic_operations/add1.java
@@ -4,7 +4,7 @@ public class add1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is for creating conflicts2");
+		int a,b,c; 
 		System.out.println("hello");
 		System.out.println("this is new line");
 
